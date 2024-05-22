b324b58ec647cde9c2fdb8cad0836cbcbb8fd1f4
muhammad Ali <ma0637579@gmail.com>
2024-05-22 23:42:44 +0500

    Update with chalk

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
+    { message: chalk.italic.greenBright("Enter your first number"), type: "number", name: "firstNumber" },
+    { message: chalk.italic.greenBright("Enter second number"), type: "number", name: "secondNumber" },
     {
-        message: "Select one of the operators to perform operation",
+        message: chalk.yellowBright("Select one of the operators to perform operation"),
         type: "list",
         name: "operator",
-        choices: ["Addition", "Subtraction", "Multiplication", "Divison"],
+        choices: [chalk.bgBlueBright("Addition"), chalk.bgCyanBright.black("Subtraction"), chalk.bgMagenta("Multiplication"), chalk.bgRedBright("Divison")],
     },
 ]);
-if (answer.operator === "Addition") {
+if (answer.operator === chalk.bgBlueBright("Addition")) {
     console.log(answer.firstNumber + answer.secondNumber);
 }
-else if (answer.operator === "Subtraction") {
+else if (answer.operator === chalk.bgCyanBright.black("Subtraction")) {
     console.log(answer.firstNumber - answer.secondNumber);
 }
-else if (answer.operator === "Multiplication") {
+else if (answer.operator === chalk.bgMagenta("Multiplication")) {
     console.log(answer.firstNumber * answer.secondNumber);
 }
-else if (answer.operator === "Divison") {
+else if (answer.operator === chalk.bgRedBright("Divison")) {
     console.log(answer.firstNumber / answer.secondNumber);
 }
 else {
-    console.log("please select  valid operator");
+    console.log(chalk.bgMagentaBright("please select  valid operator"));
 }
