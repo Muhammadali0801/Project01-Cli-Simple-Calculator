9f45a6042e84a363db8ca053eecadce3addb284e
muhammad Ali <ma0637579@gmail.com>
2024-05-30 21:16:34 +0500

    Update validation option

--- index.js
+++ index.js
@@ -1,28 +1,53 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
-const answer = await inquirer.prompt([
-    { message: chalk.italic.greenBright("Enter your first number"), type: "number", name: "firstNumber" },
-    { message: chalk.italic.greenBright("Enter second number"), type: "number", name: "secondNumber" },
-    {
-        message: chalk.yellowBright("Select one of the operators to perform operation"),
-        type: "list",
-        name: "operator",
-        choices: [chalk.bgBlueBright("Addition"), chalk.bgCyanBright.black("Subtraction"), chalk.bgMagenta("Multiplication"), chalk.bgRedBright("Divison")],
-    },
-]);
-if (answer.operator === chalk.bgBlueBright("Addition")) {
-    console.log(answer.firstNumber + answer.secondNumber);
+console.log(chalk.bgGreenBright.black("Welcome to the mshk Calculator!"));
+console.log("__________________________________________________________________________");
+async function main() {
+    let continueCalculating = true;
+    while (continueCalculating) {
+        const answer = await inquirer.prompt([
+            { message: chalk.italic.greenBright("Enter your first number"),
+                type: "input",
+                name: "firstNumber",
+                validate: input => !isNaN(input) || chalk.red("Please enter a valid number") },
+            { message: chalk.italic.greenBright("Enter second number"),
+                type: "input",
+                name: "secondNumber",
+                validate: input => !isNaN(input) || chalk.red("Please enter a valid number") },
+            {
+                message: chalk.yellowBright("Select one of the operators to perform operation"),
+                type: "list",
+                name: "operator",
+                choices: [chalk.bgBlueBright("Addition"), chalk.bgCyanBright.black("Subtraction"), chalk.bgMagenta("Multiplication"), chalk.bgRedBright("Divison")],
+            },
+        ]);
+        const firstNumber = parseFloat(answer.firstNumber);
+        const secondNumber = parseFloat(answer.secondNumber);
+        if (answer.operator === chalk.bgBlueBright("Addition")) {
+            console.log(chalk.blue(`Result: ${firstNumber + secondNumber}`));
+        }
+        else if (answer.operator === chalk.bgCyanBright.black("Subtraction")) {
+            console.log(chalk.cyan(`Result: ${firstNumber - secondNumber}`));
+        }
+        else if (answer.operator === chalk.bgMagenta("Multiplication")) {
+            console.log(chalk.magenta(`Result: ${firstNumber * secondNumber}`));
+        }
+        else if (answer.operator === chalk.bgRedBright("Divison")) {
+            console.log(chalk.red(`Result: ${firstNumber / secondNumber}`));
+        }
+        else {
+            console.log(chalk.bgMagentaBright("Please select a valid operator"));
+        }
+        const { confirm } = await inquirer.prompt({
+            type: "confirm",
+            name: "confirm",
+            message: chalk.green("Do you want to perform another calculation?"),
+            default: false,
+        });
+        continueCalculating = confirm;
+    }
+    console.log("__________________________________________________________________________");
+    console.log(chalk.bgGreenBright.black("Thank you for using the mshk Calculator!"));
 }
-else if (answer.operator === chalk.bgCyanBright.black("Subtraction")) {
-    console.log(answer.firstNumber - answer.secondNumber);
-}
-else if (answer.operator === chalk.bgMagenta("Multiplication")) {
-    console.log(answer.firstNumber * answer.secondNumber);
-}
-else if (answer.operator === chalk.bgRedBright("Divison")) {
-    console.log(answer.firstNumber / answer.secondNumber);
-}
-else {
-    console.log(chalk.bgMagentaBright("please select  valid operator"));
-}
+main();
--- index.ts
+++ index.ts
@@ -3,9 +3,24 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 
+console.log(chalk.bgGreenBright.black("Welcome to the mshk Calculator!"));
+console.log("__________________________________________________________________________");
+
+async function main() {
+  let continueCalculating = true;
+ 
+  while(continueCalculating){
+
 const answer = await inquirer.prompt([
-  { message: chalk.italic.greenBright("Enter your first number"), type: "number", name: "firstNumber" },
-  { message: chalk.italic.greenBright("Enter second number"), type: "number", name: "secondNumber" },
+  { message: chalk.italic.greenBright("Enter your first number"),
+   type: "input",
+    name: "firstNumber", 
+    validate: input => !isNaN(input) || chalk.red("Please enter a valid number")},
+
+  { message: chalk.italic.greenBright("Enter second number"),
+   type: "input",
+    name: "secondNumber",
+    validate: input => !isNaN(input) || chalk.red("Please enter a valid number")},
   {
     message: chalk.yellowBright("Select one of the operators to perform operation"),
     type: "list",
@@ -13,14 +28,33 @@ const answer = await inquirer.prompt([
     choices: [chalk.bgBlueBright("Addition"), chalk.bgCyanBright.black("Subtraction"), chalk.bgMagenta("Multiplication"),chalk.bgRedBright( "Divison")],
   },
 ]);
+
+const firstNumber = parseFloat(answer.firstNumber);
+const secondNumber = parseFloat(answer.secondNumber);
+
 if (answer.operator === chalk.bgBlueBright("Addition")) {
-  console.log(answer.firstNumber + answer.secondNumber);
+  console.log(chalk.blue(`Result: ${firstNumber + secondNumber}`));
 } else if (answer.operator === chalk.bgCyanBright.black("Subtraction")) {
-  console.log(answer.firstNumber - answer.secondNumber);
-} else if (answer.operator ===  chalk.bgMagenta("Multiplication")) {
-  console.log(answer.firstNumber * answer.secondNumber);
-} else if (answer.operator === chalk.bgRedBright( "Divison")) {
-  console.log(answer.firstNumber / answer.secondNumber);
+  console.log(chalk.cyan(`Result: ${firstNumber - secondNumber}`));
+} else if (answer.operator === chalk.bgMagenta("Multiplication")) {
+  console.log(chalk.magenta(`Result: ${firstNumber * secondNumber}`));
+} else if (answer.operator === chalk.bgRedBright("Divison")) {
+  console.log(chalk.red(`Result: ${firstNumber / secondNumber}`));
 } else {
-  console.log(chalk.bgMagentaBright("please select  valid operator"));
+  console.log(chalk.bgMagentaBright("Please select a valid operator"));
 }
+const {confirm} =await inquirer.prompt({
+  type: "confirm",
+  name: "confirm",
+  message: chalk.green("Do you want to perform another calculation?"),
+  default: false,
+});
+
+continueCalculating = confirm;
+  }
+console.log("__________________________________________________________________________");
+
+console.log(chalk.bgGreenBright.black("Thank you for using the mshk Calculator!"));
+}
+main()
+
